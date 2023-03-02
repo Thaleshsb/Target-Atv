f360aea7f42935d7f7ede8f2cd7ab264fefeb002
Thaleshsb <96483006+Thaleshsb@users.noreply.github.com>
2023-03-01 22:08:35 -0300

    Add files via upload
    Atividade 4

---
+++ Faturamento Mensal da Distribuidora.py
@@ -0,0 +1,19 @@
+SP =67836.43
+RJ =36678.66
+MG =29229.88
+ES =27165.48
+Outros =19849.53
+
+percento = (SP + RJ + MG + ES + Outros)/100.0
+
+SP = SP/percento
+RJ = RJ/percento
+MG = MG/percento
+ES = ES/percento
+Outros = Outros/percento
+
+print('SP: '+str(round(SP))+"%")
+print('RJ: '+str(round(RJ))+"%")
+print('MG: '+str(round(MG))+"%")
+print('ES: '+str(round(ES))+"%")
+print('Outros: '+str(round(Outros))+"%")
